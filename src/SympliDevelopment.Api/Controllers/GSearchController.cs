4fc27793a9ddcb82a8aa854ebeff940c99dcbd38
Verma <VermaM@aap.com.au>
2022-06-06 15:42:39 +1000

    added checks for parameters in conroller

--- src/SympliDevelopment.Api/Controllers/GSearchController.cs
+++ src/SympliDevelopment.Api/Controllers/GSearchController.cs
@@ -3,6 +3,9 @@ using SympliDevelopment.Api.Interface;
 
 namespace SympliDevelopment.Api.Controllers
 {
+    /// <summary>
+    /// Google Search Controller.
+    /// </summary>
   [ApiController]
   [Route("[controller]")]
   public class GSearchController : ControllerBase
@@ -15,8 +18,15 @@ namespace SympliDevelopment.Api.Controllers
         [HttpGet("keywords")]
         public async Task<IActionResult> GetResult([FromQuery] string url, [FromQuery] string keywords)
         {
+            if (string.IsNullOrEmpty(url))
+                return BadRequest("Url not provided");
+            else if (string.IsNullOrEmpty(keywords))
+                return BadRequest("Enter Keywords");
+            else if(!url.ToLower().Contains("http") ) // checks both http/https
+                return BadRequest("Please enter URL in format -  http(s)://www.yoursite.com.au");
             var resp =  await _gs.Search(url, keywords);
-            return Ok(keywords);
+
+            return Ok(resp);
         }
     
   }
